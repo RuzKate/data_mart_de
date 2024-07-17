90d162202ded10c2a27350d35a3fa73275f3ea53
Екатерина <140340116+RuzKate@users.noreply.github.com>
2024-07-17 15:12:30 +0300

    Update connect.py

--- scripts/connect.py
+++ scripts/connect.py
@@ -2,6 +2,23 @@ import psycopg2
 
 class DatabaseConnect:
     def __init__(self, db_name, db_user, db_password, db_host, db_port):
+        """Инициализатор класса
+
+            :param db_name: название базы данных
+            :type db_name: str
+
+            :param db_user: имя пользователя для подключения к бд
+            :type db_user: str
+
+            :param db_password: пароль пользователя для подключения к бд
+            :type db_password: str
+
+            :param db_host: адрес сервера базы данных
+            :type db_host: str
+
+            :param db_port: порт для подключения
+            :type db_port: str
+        """
         self.db_name = db_name
         self.db_user = db_user
         self.db_password = db_password
@@ -11,26 +28,7 @@ class DatabaseConnect:
 
 
     def connect_to_db(self):
-        """Подключение к серверу PostgreSQL
-
-                :param db_name: название базы данных
-                :type db_name: str
-
-                :param db_user: имя пользователя для подключения к бд
-                :type db_user: str
-
-                :param db_password: пароль пользователя для подключения к бд
-                :type db_password: str
-
-                :param db_host: адрес сервера базы данных
-                :type db_host: str
-
-                :param db_port: порт для подключения
-                :type db_port: str
-                
-                :rtype: объект connection
-                :return: объект подключения
-            """
+        """Подключение к серверу PostgreSQL"""
         try:
             self.connection = psycopg2.connect(
                 dbname=self.db_name,
@@ -43,5 +41,6 @@ class DatabaseConnect:
             print(f'Unable to connect to the server: {error}')
 
     def disconnect(self):
+        """Закрытие подключения"""
         if self.connection:
             self.connection.close()
